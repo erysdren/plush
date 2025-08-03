02d85bfaa976fa8b74a80a9618fe0ffd60d668b4
erysdren <contact@erysdren.me>
2025-08-03 06:33:26 -0500

    add teapot example

---
+++ examples/teapot.c
@@ -0,0 +1,94 @@
+// teapot.c: OBJ model loading example
+// owo
+
+#include <float.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <plush/plush.h>
+
+#include "ex.h"
+
+pl_Light *light;
+pl_Obj *model;
+pl_Mat *material;
+pl_Cam *camera;
+uint8_t framebuffer[W * H];
+float zbuffer[W * H];
+uint8_t palette[768];
+
+int main(int argc, char **argv)
+{
+	/* setup graphics mode */
+	exSetGraphics();
+
+	/* create material */
+	/* these ambient/diffuse/specular values are taken from Haiku's GLTeapot */
+	material = plMatCreate();
+	material->NumGradients = 50;
+	material->ShadeType = PL_SHADE_GOURAUD;
+	material->Ambient[0] = 0.1745 * 255;
+	material->Ambient[1] = 0.03175 * 255;
+	material->Ambient[2] = 0.03175 * 255;
+	material->Diffuse[0] = 0.61424 * 255;
+	material->Diffuse[1] = 0.10136 * 255;
+	material->Diffuse[2] = 0.10136 * 255;
+	material->Specular[0] = 0.727811 * 255;
+	material->Specular[1] = 0.626959 * 255;
+	material->Specular[2] = 0.626959 * 255;
+	plMatInit(material);
+
+	/* create palette */
+	plMatMakeOptPal(palette, 1, 255, &material, 1);
+	palette[0] = palette[1] = palette[2] = 0;
+	plMatMapToPal(material, palette, 0, 255);
+
+	exSetPalette(palette);
+
+	/* load fork model */
+	model = plReadWavefrontObj("teapot.obj", material);
+	plObjScale(model, 32);
+	plObjTranslate(model, 0, -32, 0);
+
+	/* create camera */
+	camera = plCamCreate(W, H, W * 3.0 / (H * 4.0), 90.0, framebuffer, zbuffer);
+	camera->Z = -300;
+
+	/* create light */
+	light = plLightSet(plLightCreate(), PL_LIGHT_VECTOR, 0.0, 0.0, 0.0, 1.0, 1.0);
+
+	/* main loop */
+	while (!exGetKey())
+	{
+		/* rotate model */
+		model->Xa += 1.0;
+		model->Ya += 1.0;
+		model->Za += 1.0;
+
+		/* clear back buffer */
+		memset(zbuffer, 0, sizeof(zbuffer));
+		memset(framebuffer, 0, sizeof(framebuffer));
+
+		/* render frame */
+		plRenderBegin(camera);
+		plRenderLight(light);
+		plRenderObj(model);
+		plRenderEnd();
+
+		/* wait for vsync, then copy to screen */
+		exWaitVSync();
+		memcpy(exGraphMem, framebuffer, sizeof(framebuffer));
+	}
+
+	/* clean up */
+	plCamDelete(camera);
+	plLightDelete(light);
+	plObjDelete(model);
+	plMatDelete(material);
+
+	/* shut down video */
+	exSetText();
+
+	return 0;
+}
