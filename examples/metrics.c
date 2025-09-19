dc3083273ecbef3b20373fcaea66b38fce680db4
Cyan <cyanbun96@gmail.com>
2025-09-19 10:24:51 +0300

    Basic frametime graph for metrics.c

--- examples/metrics.c
+++ examples/metrics.c
@@ -23,17 +23,50 @@ void drawFPS() {
 	static int old_framecount = 0;
 	static int old_tick = 0;
 	static uint8_t string[16] = "... FPS";
+
 	if (exClock() - old_tick > exClockPerSecond()) {
 		old_tick = exClock();
 		int frames_drawn = framecount - old_framecount;
 		old_framecount = framecount;
 		snprintf(string, sizeof(string), "%d FPS", frames_drawn);
 	}
-	framecount++;
+	framecount++; /* remove if incremented globally */
+
 	int xpos = camera->ClipLeft + 5;
 	int ypos = camera->ClipTop + 5;
 	int color = 50;
 	plTextPrintf(camera, xpos, ypos, 0, color, string);
+}
+
+void drawFrameGraph() {
+	static int framecount = 0; /* replace with a global if you have one */
+	static int frametimes[100];
+	static int time_n = sizeof(frametimes) / sizeof(int);
+	static int old_tick = 0;
+
+	float scale = 2.5; /* applied before clipping */
+	int clip = 200; /* to prevent the graph from going too far right */
+
+	int frametime = exClock() - old_tick;
+	old_tick = exClock();
+	frametimes[framecount % time_n] = (int)(frametime * scale) % 200;
+	framecount++; /* remove if incremented globally */
+
+	int xpos = camera->ClipLeft + 5;
+	int ypos = camera->ClipTop + 25;
+	int color = 50;
+	float fade_speed = 2;
+	for (int i = 0; i < time_n; i++) { /* right-to-left, bright-to-dark */
+		int len = color * fade_speed < frametimes[i] ?
+			color * fade_speed : frametimes[i];
+		float cur_color = color;
+		for (int j = 0; j < len; j++) {
+			int xcoord = xpos + len - j;
+			int ycoord = W * (ypos + i);
+			framebuffer[xcoord + ycoord] = cur_color;
+			cur_color -= fade_speed;
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -96,6 +129,7 @@ int main(int argc, char **argv)
 		plRenderEnd();
 
 		drawFPS();
+		drawFrameGraph();
 		/*float dummy = 0; // time waster loop 
 		for (int i = 1; i < 10000000; i++)
 			dummy += sqrt(i);*/
