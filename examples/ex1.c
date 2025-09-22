6e2385f9f3b43bd7afd9588f78b1b8a251065110
erysdren <contact@erysdren.me>
2025-09-22 05:35:23 -0500

    examples: ex1: overhaul

--- examples/ex1.c
+++ examples/ex1.c
@@ -1,85 +1,77 @@
 // Ex1.c: simple Plush example
 // Rotates a flat shaded cube
 
-#include <float.h>
-#include <time.h>
-#include <stdio.h>
-#include <math.h>
-
-// Include the plush header file
-#include <plush/plush.h>
-
-// Include our example graphics interface module
 #include "ex.h" 
 
 // Our variables
-pl_Light *TheLight;   // Our light
-pl_Obj *TheCube;      // Our cube object
-pl_Mat *CubeMat;      // The material for the cube
-pl_Mat *AllMaterials[2]; // Used for creating palette
-pl_Cam *TheCamera; // Our camera
-uint8_t TheFrameBuffer[W*H]; // Our framebuffer to render to
-uint8_t ThePalette[768];
+static pl_Light *TheLight; // Our light
+static pl_Obj *TheCube; // Our cube object
+static pl_Mat *CubeMat; // The material for the cube
+static pl_Cam *TheCamera; // Our camera
+static uint8_t TheFrameBuffer[W*H]; // Our framebuffer to render to
+static uint8_t ThePalette[768];
 
-int main(int argc, char **argv) { // Main
-#if defined(DJGPP) || defined(__WATCOMC__)
-   // Put the fpu in a low precision, no exception state
-  _control87(MCW_EM|PC_24,MCW_EM|MCW_PC); 
-#endif
+int exInit(void **appstate, int argc, char **argv)
+{
+	CubeMat = plMatCreate(); // Create the material for the cube
+	CubeMat->NumGradients = 100; // Have it use 100 colors
+	CubeMat->ShadeType = PL_SHADE_FLAT; // Make the cube flat shaded
+	plMatInit(CubeMat); // Initialize the material
 
-  exSetGraphics(); // Set graphics
+	plMatMakeOptPal(ThePalette,1,255,&CubeMat,1); // Create a nice palette
 
-  CubeMat = plMatCreate();    // Create the material for the cube
-  CubeMat->NumGradients = 100; // Have it use 100 colors
-  CubeMat->ShadeType = PL_SHADE_FLAT; // Make the cube flat shaded
-  plMatInit(CubeMat);          // Initialize the material
+	ThePalette[0] = ThePalette[1] = ThePalette[2] = 0; // Color 0 is black
 
-  AllMaterials[0] = CubeMat; // Make list of materials
-  AllMaterials[1] = 0; // Null terminate list of materials
-  plMatMakeOptPal(ThePalette,1,255,AllMaterials,1); // Create a nice palette
+	plMatMapToPal(CubeMat,ThePalette,0,255); // Map the material to our palette
 
-  ThePalette[0] = ThePalette[1] = ThePalette[2] = 0; // Color 0 is black
+	exSetPalette(ThePalette); // Set the palette
 
-  plMatMapToPal(CubeMat,ThePalette,0,255); // Map the material to our palette
+	TheCube = plObjCreate(NULL);
+	TheCube->Model = plMakeBox(100.0,100.0,100.0,CubeMat); // Create the cube
 
-  // Convert std 8 bit/chan palette to vga's 6 bit/chan palette
-  // for (i = 0; i < 768; i ++) ThePalette[i] >>= 2;
-  exSetPalette(ThePalette); // Set the palette
- 
-  TheCube = plObjCreate(NULL);
-  TheCube->Model = plMakeBox(100.0,100.0,100.0,CubeMat); // Create the cube
+	// Create the camera
+	TheCamera = plCamCreate(W, // Screen width
+							H, // Screen height
+							W*3.0/(H*4.0), // Aspect ratio
+							90.0, // Field of view
+							TheFrameBuffer, // Framebuffer
+							NULL); // ZBuffer (none)
+	TheCamera->Z = -300; // Back the camera up from the origin
 
-  TheCamera = plCamCreate(W, // Screen width
-                          H, // Screen height
-                          W*3.0/(H*4.0), // Aspect ratio
-                          90.0, // Field of view
-                          TheFrameBuffer, // Framebuffer
-                          NULL // ZBuffer (none)
-                          ); // Create the camera
-  TheCamera->Z = -300; // Back the camera up from the origin
+	TheLight = plLightSet(plLightCreate(), // Create a light to be set up
+							PL_LIGHT_VECTOR, // vector light
+							0.0,0.0,0.0, // rotation angles
+							1.0, // intensity
+							1.0); // falloff, not used for vector lights
+	return PL_EXIT_CONTINUE;
+}
 
-  TheLight = plLightSet(plLightCreate(), // Create a light to be set up
-             PL_LIGHT_VECTOR, // vector light
-             0.0,0.0,0.0, // rotation angles
-             1.0, // intensity
-             1.0); // falloff, not used for vector lights
-             
-  while (!exGetKey()) { // While the keyboard hasn't been touched
-    TheCube->Xa += 1.0; // Rotate by 1 degree on each axis
-    TheCube->Ya += 1.0;
-    TheCube->Za += 1.0;
-    memset(TheFrameBuffer,0,W*H); // clear framebuffer for next frame
-    plRenderBegin(TheCamera);        // Start rendering with the camera
-    plRenderLight(TheLight);         // Render our light
-    plRenderObj(TheCube);            // Render our object
-    plRenderEnd();                   // Finish rendering
-    exWaitVSync();                   // Sync with retrace
-    memcpy(exGraphMem,TheFrameBuffer,W*H); // dump to screen
-  }
-  plMatDelete(CubeMat);
-  plObjDelete(TheCube);
-  plCamDelete(TheCamera);
-  plLightDelete(TheLight);
-  exSetText(); // Restore text mode
-  return 0;          // Quit
+int exIterate(void *appstate)
+{
+	TheCube->Xa += 1.0; // Rotate by 1 degree on each axis
+	TheCube->Ya += 1.0;
+	TheCube->Za += 1.0;
+	memset(TheFrameBuffer,0,W*H); // clear framebuffer for next frame
+	plRenderBegin(TheCamera); // Start rendering with the camera
+	plRenderLight(TheLight); // Render our light
+	plRenderObj(TheCube); // Render our object
+	plRenderEnd(); // Finish rendering
+	exWaitVSync(); // Sync with retrace
+	memcpy(exGraphMem,TheFrameBuffer,W*H); // dump to screen
+	return PL_EXIT_CONTINUE;
+}
+
+int exKeyEvent(void *appstate, int key)
+{
+	// any keypress will trigger an exit
+	return PL_EXIT_SUCCESS;
+}
+
+void exQuit(void *appstate, int code)
+{
+	plMatDelete(CubeMat);
+	plMdlDelete(TheCube->Model);
+	plObjDelete(TheCube);
+	plCamDelete(TheCamera);
+	plLightDelete(TheLight);
 }
